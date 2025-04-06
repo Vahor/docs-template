a3e999f74cf0e07b501a220f910dee01303b1e4e
Vahor <me@vahor.fr>
2025-04-06 23:03:24 +0200

    oui

--- src/app/(main)/components/banner.tsx
+++ src/app/(main)/components/banner.tsx
@@ -25,7 +25,7 @@ export function BannerContainer() {
 
 function Banner({ onClose }: BannerProps) {
   return (
-    <div className="fixed top-0 inset-x-0 bg-bg-white px-4 py-2 text-text-dark h-[var(--banner-height)] flex items-center dark z-[50]">
+    <div className="fixed top-0 inset-x-0 bg-bg-white px-4 py-2 text-text-dark h-[var(--banner-height)] flex items-center dark z-[50]" style={{ paddingRight: "var(--removed-body-scroll-bar-size)" }}>
       <div className="flex items-center justify-center gap-2 text-sm font-medium w-full">
         <span>🎉 Wow nice banner.</span>
         <Link href="#" className="underline underline-offset-4 hover:opacity-90">
